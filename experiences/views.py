0b7901c47d35cf021beb8570647aebb2e71136bb
hazel0703 <apdr83@gmail.com>
2021-06-15 01:42:25 +0200

    Updating and adding new urls and views

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# Django REST Framework
+from rest_framework import mixins, status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsStandardUser
+
+#Serializers
+from experiences.serializers import (ExperienceModelSerializer, ExperienceSerializer)
+
+class ExperienceViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+
+    serializer_class = ExperienceModelSerializer
+
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated, IsStandardUser]
+        return [permission() for permission in permission_classes]
+
+        
+    def create(self, request, *args, **kwargs):
+        serializer = ExperienceSerializer(data=request.data, context={"request": self.request})
+        serializer.is_valid(raise_exception=True)
+        exp = serializer.save()
+        data = ExperienceModelSerializer(exp).data
+        return Response(data, status=status.HTTP_201_CREATED)
+        
--- users/serializers.py
+++ users/serializers.py
@@ -2,10 +2,13 @@
 
 # Django
 from django.contrib.auth import password_validation, authenticate
+from django.core.validators import RegexValidator, FileExtensionValidator
+
 
 # Django REST Framework
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
 # Models
 from users.models import User
@@ -22,25 +25,89 @@ class UserModelSerializer(serializers.ModelSerializer):
             'email',
         )
 
+#Class for Login
+
 class UserLoginSerializer(serializers.Serializer):
 
-    # Campos que vamos a requerir
+    # Fields that are required
     email = serializers.EmailField()
     password = serializers.CharField(min_length=8, max_length=64)
 
-    # Primero validamos los datos
+    # Validating the data
     def validate(self, data):
 
-        # authenticate recibe las credenciales, si son válidas devuelve el objeto del usuario
+        # authenticate get the credentials, If they are valid then the object user comes  
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
-            raise serializers.ValidationError('Las credenciales no son válidas')
+            raise serializers.ValidationError('Credentials are not valid')
 
-        # Guardamos el usuario en el contexto para posteriormente en create recuperar el token
+        # User is saved in the context for later on, in create retrieve the token
         self.context['user'] = user
         return data
 
     def create(self, data):
-        """Generar o recuperar token."""
+        """get or retrieve token."""
         token, created = Token.objects.get_or_create(user=self.context['user'])
         return self.context['user'], token.key
+
+#Creating a class for the signup
+
+class UserSignUpSerializer(serializers.Serializer):
+
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        min_length=4,
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    photo = serializers.ImageField(
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])], 
+        required=False
+    )
+
+    extract = serializers.CharField(max_length=1000, required=False)
+
+    city = serializers.CharField(max_length=250, required=False)
+
+    country = serializers.CharField(max_length=250, required=False)
+
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Introduce a number as it follows: +999999999. The limit is 15 digits."
+    )
+    phone = serializers.CharField(validators=[phone_regex], required=False)
+
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
+
+    first_name = serializers.CharField(min_length=2, max_length=50)
+    last_name = serializers.CharField(min_length=2, max_length=100)
+
+
+# This def will validate the password, defining a limit for the size of the photo to be upload and save the user
+# UserSignUpSerializer.
+
+    def validate(self, data):
+        passwd = data['password']
+        passwd_conf = data['password_confirmation']
+        if passwd != passwd_conf:
+            raise serializers.ValidationError("Password input are not the same")
+        password_validation.validate_password(passwd)
+
+        image = None
+        if 'photo' in data:
+            image = data['photo']
+
+        if image:
+            if image.size > (512 * 1024):
+                raise serializers.ValidationError(f"The image is too big, max size is 512KB and actual photo size is {round(image.size / 1024)}KB")
+
+        return data
+
+    def create(self, data):
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        return user
