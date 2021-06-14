df3ce61059e635089d4833f88ce562770022065d
hazel0703 <apdr83@gmail.com>
2021-06-14 23:57:17 +0200

    adding serializers.py and working on the views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+"""Users views."""
 
-# Create your views here.
+# Django REST Framework
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+# Serializers
+from users.serializers import UserLoginSerializer, UserModelSerializer
+
+# Models
+from users.models import User
+
+class UserViewSet(viewsets.GenericViewSet):
+
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserModelSerializer
+
+    # Detail define si es una petición de detalle o no, en methods añadimos el método permitido, en nuestro caso solo vamos a permitir post
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User sign in."""
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, token = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
